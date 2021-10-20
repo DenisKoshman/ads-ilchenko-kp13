05927f74b9b1a41af9d8a5d810803b5336a8c3dd
DenisKoshman <92885243+DenisKoshman@users.noreply.github.com>
2021-10-20 22:12:54 +0300

    Add files via upload

---
+++ Лабороторні/Лабороторная №1/АСД_2_Задача/АСД_2_Задача/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+using static System.Console;
+
+namespace АСД_2_Задача
+{
+    class Program
+    {
+
+        public static bool isPrime(double x)
+        {
+            double sqrtX = Math.Sqrt(x);
+            for (int i = 2; i <= sqrtX; i++)
+                if (x % i == 0) 
+                    return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                int n = Convert.ToInt32(ReadLine());
+                int pow = 1;
+
+                for (int i = 2; (i - 1) <= n; i *= 2, pow++)
+                {
+                    bool isPrimeX = false;
+                    isPrimeX = isPrime(i - 1);
+
+                    if (isPrimeX)
+                    {
+                        isPrimeX = false;
+                        isPrimeX = isPrime(pow);
+
+                        if (isPrimeX)
+                        {
+                            WriteLine(i - 1);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                WriteLine("Not a number or n < 1");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
